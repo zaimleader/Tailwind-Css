63ea2129c574f3d75e1bbe0dde6d921e1b1a4250
zaimleader <zaiml.leader23@gmail.com>
2024-05-05 18:46:25 +0100

    Tailwind Css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  cssnano: {}
+}
+
